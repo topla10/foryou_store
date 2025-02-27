1d55b15a6b125af5cc9af84b5bab266c84c2f2f9
Maxime <gilotmaxime@outlook.com>
2025-02-26 22:01:19 -0800

    Start working with the api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import { useEffect } from 'react'
 import './App.css'
+import { TheHeader } from './TheHeader'
 
 function App() {
-    return (
+
+  async function fetchData() {
+    try {
+      const response = await fetch('https://dummyjson.com/products')
+      if(!response.ok) {
+        throw new Error(`Status: ${response.status}`)
+      }
+      const data = await response.json()
+      console.log(data)
+    } catch (error) {
+      console.log(`Fetch Error: ${error.message}`)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  return (
+    <>
+      <TheHeader/>
       <h1>Hi there</h1>
-    )
+    </>
+  )
     
   
 }
---
+++ src/TheHeader.jsx
@@ -0,0 +1,8 @@
+import './theHeader.css'
+export function TheHeader() {
+    return(
+        <div className='theHeader'> 
+            <h1>Heyyyy</h1>
+        </div>
+    )
+}
